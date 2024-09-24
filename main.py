c40dc018ead0b007dd39fcb514c3e1f5473f1559
TonyBimenyi <tonyblaisb@gmail.com>
2024-09-24 15:45:30 +0800

    add initializations

--- main.py
+++ main.py
@@ -6,3 +6,33 @@ import matplotlib.pyplot as plt
 rho = 0.3
 lambda_ = 0.5
 eta = 0.5
+mu = 1
+epsilon = 10^-4
+
+L = 500
+
+# Initialize arrays
+phi1 = np.zeros((L, 1))
+phi2 = np.zeros((L, 1))
+phi3 = np.zeros((L, 1))
+phi4 = np.zeros((L, 1))
+
+u1 = np.zeros((L, 1))
+u2 = np.zeros((L, 1))
+u3 = np.zeros((L, 1))
+u4 = np.zeros((L, 1))
+
+y1 = np.zeros((L + 1, 1))
+y2 = np.zeros((L + 1, 1))
+y3 = np.zeros((L + 1, 1))
+y4 = np.zeros((L + 1, 1))
+
+e1 = np.zeros((L + 1, 1))
+e2 = np.zeros((L + 1, 1))
+e3 = np.zeros((L + 1, 1))
+e4 = np.zeros((L + 1, 1))
+
+si1 = np.zeros((L, 1))
+si2 = np.zeros((L, 1))
+si3 = np.zeros((L, 1))
+si4 = np.zeros((L, 1))
