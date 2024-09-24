fddceb7fdb4c826de6e01a32e125aa69a7adaf68
TonyBimenyi <tonyblaisb@gmail.com>
2024-09-24 15:55:10 +0800

    starting with model

--- main.py
+++ main.py
@@ -27,6 +27,9 @@ y2 = np.zeros((L + 1, 1))
 y3 = np.zeros((L + 1, 1))
 y4 = np.zeros((L + 1, 1))
 
+w5 = np.zeros((L + 1, 1))
+w6 = np.zeros((L + 1, 1))
+
 e1 = np.zeros((L + 1, 1))
 e2 = np.zeros((L + 1, 1))
 e3 = np.zeros((L + 1, 1))
@@ -55,3 +58,28 @@ for k in range(1, L-1):
         phi2[k] = phi2[k - 1] + (eta * (u2[k - 1] - u2[k - 2]) / (mu + (abs(u2[k - 1] - u2[k - 2]))**2)) * (y2[k] - y2[k - 1] - phi2[k - 1] * (u2[k - 1] - u2[k - 2]))
         phi3[k] = phi3[k - 1] + (eta * (u3[k - 1] - u3[k - 2]) / (mu + (abs(u3[k - 1] - u3[k - 2]))**2)) * (y3[k] - y3[k - 1] - phi3[k - 1] * (u3[k - 1] - u3[k - 2]))
         phi4[k] = phi4[k - 1] + (eta * (u4[k - 1] - u4[k - 2]) / (mu + (abs(u4[k - 1] - u4[k - 2]))**2)) * (y4[k] - y4[k - 1] - phi4[k - 1] * (u4[k - 1] - u4[k - 2]))
+
+    
+    si1[k] = -y1[k] + w5[k] + y2[k] + y3[k]
+    si2[k] = y1[k] - y2[k] + y3[k] + y4[k]
+    si3[k] = y1[k] + y2[k] - 2 * y3[k] + y4[k]
+    si4[k] = y2[k] + y3[k] -y4[k] + w6[k]
+
+    if k == 1:
+        u1[0] = 0
+        u2[0] = 0
+        u3[0] = 0
+        u4[0] = 0
+    else:
+        u1[k] = u1[k - 1] + (rho * phi1[k]) / (lambda_ + abs(phi1[k])**2) * si1[k]
+        u2[k] = u2[k - 1] + (rho * phi2[k]) / (lambda_ + abs(phi2[k])**2) * si2[k]
+        u3[k] = u3[k - 1] + (rho * phi3[k]) / (lambda_ + abs(phi3[k])**2) * si3[k]
+        u4[k] = u4[k - 1] + (rho * phi4[k]) / (lambda_ + abs(phi4[k])**2) * si4[k]
+
+    y1[0] = 0
+    y2[0] = 0
+    y3[0] = 0
+    y4[0] = 0
+
+    y1[k+1] = 0.7 * y1[k] + u1[k]
+    y2[k+1] = 0.6 * 
