04aa24bb8cce48f6412e83b9a2e00624b52cdf4e
TonyBimenyi <tonyblaisb@gmail.com>
2024-10-31 15:25:30 +0800

    inputs

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import rcParams
-
-# Setting the font to Poppins (Ensure it's installed on your system)
-# rcParams['font.family'] = 'times new roman'
 
 # Define other parameters
 rho = 0.3
@@ -65,26 +61,10 @@ def simulate(lambda_):
             phi3[k] = phi3[k - 1] + (eta * (u3[k - 1] - u3[k - 2]) / (mu + (abs(u3[k - 1] - u3[k - 2]))**2)) * (y3[k] - y3[k - 1] - phi3[k - 1] * (u3[k - 1] - u3[k - 2]))
             phi4[k] = phi4[k - 1] + (eta * (u4[k - 1] - u4[k - 2]) / (mu + (abs(u4[k - 1] - u4[k - 2]))**2)) * (y4[k] - y4[k - 1] - phi4[k - 1] * (u4[k - 1] - u4[k - 2]))
 
-        # si1 to si4 updates
-          # si1[k] = -y1[k] + w5[k] + y2[k] 
-        # si2[k] = y1[k] - y2[k] + y3[k] + y4[k]
-        # si3[k] = y1[k] + y2[k] - 2 * y3[k] + y4[k]
-        # si4[k] = y2[k] + y3[k] - y4[k] + w6[k]
-
-        # si1[k] = y1[k] - y3[k] - w5[k]
-        # si2[k] = - y1[k] + 2 * y2[k] - y4[k]
-        # si3[k] = - y2[k] + y3[k]
-        # si4[k] = - y3[k] + y4[k] -w6[k] 
-
-        # si1[k] = -y1[k] + y2[k] -w5[k]
-        # si2[k] = -y2[k] + y3[k]
-        # si3[k] = y1[k] - 2 * y3[k] + y4[k]
-        # si4[k] = y2[k] - y4[k] - w6[k]
-
         si1[k] = y2[k] - 2 * y1[k] + w5[k]
         si2[k] = y3[k] - y2[k]
         si3[k] = y4[k] - 2 * y3[k] + y1[k]
-        si4[k] = y2[k] - 2* y4[k] + w6[k]
+        si4[k] = y2[k] - 2 * y4[k] + w6[k]
 
         if k == 1:
             u1[1] = 0
@@ -102,35 +82,54 @@ def simulate(lambda_):
         y3[1] = 0.6
         y4[1] = 0.4
 
-        y1[k+1] = 0.7 * y1[k] + u1[k]
-        y2[k+1] = 0.6 * np.cos(y2[k]) + 0.3 * y2[k] + u2[k]
-        y3[k+1] = (y3[k]) / (1 + y3[k]**2) + u3[k]
-        y4[k+1] = (y4[k]) / (1 + y4[k]**2) + u4[k]**3
+        y1[k + 1] = 0.7 * y1[k] + u1[k]
+        y2[k + 1] = 0.6 * np.cos(y2[k]) + 0.3 * y2[k] + u2[k]
+        y3[k + 1] = (y3[k]) / (1 + y3[k]**2) + u3[k]
+        y4[k + 1] = (y4[k]) / (1 + y4[k]**2) + u4[k]**3
 
-    return y1, y2, y3, y4, w5, w6
+    return y1, y2, y3, y4, w5, w6, u1, u2, u3, u4
 
-# Different values of lambda_
+# Simulate for multiple lambda values for outputs and inputs
 lambda_values = [5, 3, 2, 0.5]
-lamda =[2]
-fig, axs = plt.subplots(2, 2, figsize=(15, 10))
+outputs = {}
+for lambda_value in lambda_values:
+    y1, y2, y3, y4, w5, w6, u1, u2, u3, u4 = simulate(lambda_value)
+    outputs[lambda_value] = (y1, y2, y3, y4, w5, w6, u1, u2, u3, u4)
 
-for i, lambda_ in enumerate(lambda_values):
-    y1, y2, y3, y4, w5, w6 = simulate(lambda_)
-    
-    ax = axs[i//2, i%2]
+# Plot y1 to y4 for each lambda value (Outputs)
+fig, axs = plt.subplots(2, 2, figsize=(15, 10))
+for i, (lambda_value, (y1, y2, y3, y4, w5, w6, _, _, _, _)) in enumerate(outputs.items()):
+    ax = axs[i // 2, i % 2]
     ax.plot(y1[:-1], label="agent1", color='orange', linestyle='solid')
     ax.plot(y2[:-1], label="agent2", color='green', linestyle='solid')
     ax.plot(y3[:-1], label="agent3", color='red', linestyle='solid')
     ax.plot(y4[:-1], label="agent4", color='purple', linestyle='solid')
-    ax.plot(w5, label="agent5(Leader 1)", color='blue', linestyle='dashed', drawstyle='steps-post')
-    ax.plot(w6, label="agent6(Leader 2)", color='blue', linestyle='dashed', drawstyle='steps-post')
+    ax.plot(w5, label="agent5 (Leader 1)", color='blue', linestyle='dashed', drawstyle='steps-post')
+    ax.plot(w6, label="agent6 (Leader 2)", color='blue', linestyle='dashed', drawstyle='steps-post')
     
-    ax.set_title(f"$\lambda$ = {lambda_}")
+    ax.set_title(f"Outputs for $\lambda$ = {lambda_value}")
     ax.grid(True)
     ax.set_xlabel("time step", fontsize=12)
     ax.set_ylabel("outputs", fontsize=12)
     ax.legend(fontsize=10)
 
+plt.tight_layout()
+plt.show()
+
+# Plot u1 to u4 for each lambda value (Inputs)
+fig, axs = plt.subplots(2, 2, figsize=(15, 10))
+for i, (lambda_value, (_, _, _, _, _, _, u1, u2, u3, u4)) in enumerate(outputs.items()):
+    ax = axs[i // 2, i % 2]
+    ax.plot(u1, label="u1", color='orange', linestyle='solid')
+    ax.plot(u2, label="u2", color='green', linestyle='solid')
+    ax.plot(u3, label="u3", color='red', linestyle='solid')
+    ax.plot(u4, label="u4", color='purple', linestyle='solid')
+
+    ax.set_title(f"Inputs for $\lambda$ = {lambda_value}")
+    ax.grid(True)
+    ax.set_xlabel("time step", fontsize=12)
+    ax.set_ylabel("inputs", fontsize=12)
+    ax.legend(fontsize=10)
 
 plt.tight_layout()
 plt.show()
